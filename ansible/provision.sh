a347891951f259d1c3b197216b08e7488599e2c8
Mariusz Kraj <mariusz.kraj@gmail.com>
2015-05-29 15:48:29 +0200

    Fixed provision path

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -4,4 +4,4 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
 
-ansible-playbook --connection=local --inventory=/usr/share/nginx/www/app/ansible/hosts /usr/share/nginx/www/app/ansible/playbook.yml
+ansible-playbook --connection=local --inventory=/vagrant/ansible/hosts /vagrant/ansible/playbook.yml
