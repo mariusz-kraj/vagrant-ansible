c5e61881a64356495b3c9565f0e7f8a9d3a3d7ac
Mariusz Kraj <mariusz.kraj@gmail.com>
2015-06-04 20:20:22 +0200

    Fix

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -4,4 +4,4 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
 
-ansible-playbook --connection=local --inventory=/vagrant/ansible/hosts /vagrant/ansible/playbook.yml
+ansible-playbook -v --connection=local --inventory=/usr/share/nginx/www/app/vagrant-ansible/ansible/hosts /usr/share/nginx/www/app/vagrant-ansible/ansible/playbook.yml
