0493f5d6a9b9d042e6cc84ad530ee22a42c0b128
Mariusz Kraj <mariusz.kraj@gmail.com>
2015-09-15 12:12:32 +0200

    Mailcatcher

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -2,6 +2,9 @@
 
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install ansible -y
 
 ansible-playbook -v --connection=local --inventory=/usr/share/nginx/www/app/vagrant-ansible/ansible/hosts /usr/share/nginx/www/app/vagrant-ansible/ansible/playbook.yml
+
+sudo apt-get autoremove -y
